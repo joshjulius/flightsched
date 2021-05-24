c9cd4e87209d550a7a87e8c3470c502a3e6a826f
lyjchan0411 <lyjchan0411@gmail.com>
2021-05-23 22:31:54 -0400

    added post method for user endpoint

--- server/routes/api/users.route.js
+++ server/routes/api/users.route.js
@@ -13,4 +13,36 @@ router.get("/", (req, res) => {
       console.log("Users GET method failed");
     });
 });
+
+router.get("/:id", (req, res) => {
+  User.findById(req.params._id)
+    .then((result) => {
+      res.send(result);
+      console.log("User GET id Method Success");
+    })
+    .catch((err) => {
+      console.log("User GET id Method failed");
+    });
+});
+
+router.post("/", (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    phone: req.body.phone,
+    userName: req.body.userName,
+    email: req.body.email,
+    role: req.body.role,
+    lastFlight: req.body.lastFlight,
+  });
+
+  user
+    .save()
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 export default router;
