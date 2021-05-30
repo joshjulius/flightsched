1b2101b16c950a417fc25c6fc335751144b79392
Joshua Julius <joshjulius1@gmail.com>
2021-05-30 02:10:02 -0500

    add modal component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
+import './App.scss';
+import "./normalize.css";
 
 import Modal from "./components/Modal/Modal";
 import Navbar from "./components/Navbar/Navbar";
-import Schedule from "./components/Schedule/Schedule";
+// import Schedule from "./components/Schedule/Schedule";
 
 function App() {
+
+  const [visibility, setVisibility] = useState(false);
+
+  const showModal = () => {
+    setVisibility(true);
+  }
+
+  const hideModal = () => {
+    setVisibility(false);
+  }
+
   return (
     <div className="App">
-      <Modal />
       <Navbar />
-      <Schedule />
+      <button onClick={showModal}>Create a Reservation</button>
+      <Modal visibility={visibility} hideModal={hideModal} />
+      {/* <Schedule /> */}
     </div>
   );
+
 }
 
 export default App;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Navbar.scss";
 
 const Navbar = () => {
     return(
-        <>
+        <div className="navbar">
             <p>Hamburger Icon</p>
             <p>Flight School Name</p>
             <p>Help Button</p>
             <p>User name with dropdown menu</p>
-        </>
+        </div>
     );
 }
 
