ffe0ede6dc2119f57dd8e3685b432ac151a81e69
lyjchan0411 <lyjchan0411@gmail.com>
2021-06-02 17:24:33 -0400

    working on increment/decrement function

--- client/src/components/Optionbar/Optionbar.jsx
+++ client/src/components/Optionbar/Optionbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Optionbar.scss";
 import arrowLeft from "../../assets/icons/left-arrow.svg";
 import arrowRight from "../../assets/icons/right-arrow.svg";
@@ -9,6 +9,17 @@ export default function Optionbar() {
     year: "numeric",
     month: "long",
     day: "numeric",
+  };
+
+  const date = new Date();
+  const [dateDisplay, setDateDisplay] = useState(
+    date.toLocaleString("en-US", dateOption)
+  );
+
+  const dateIncrement = () => {
+    date.setDate(date.getDate() + 1);
+    console.log(date);
+    setDateDisplay(date.toLocaleString("en-US", dateOption));
   };
 
   return (
@@ -22,16 +33,17 @@ export default function Optionbar() {
               className="optionbar__arrow"
             />
           </button>
-          <div>{new Date().toLocaleString("en-US", dateOption)}</div>
+          <div className="optionbar__date">{dateDisplay}</div>
           <button className="optionbar__btn">
             <img
               src={arrowRight}
               alt="arrow pointing right"
               className="optionbar__arrow"
+              onClick={dateIncrement}
             />
           </button>
-          <div>Today</div>
-          <select>
+          <div className="optionbar__top-date optionbar__top-option">Today</div>
+          <select className="optionbar__direction-option optionbar__top-option">
             <option value="horizontal">Day Horizontal</option>
             <option value="vertical">Day Vertical</option>
           </select>
@@ -43,29 +55,29 @@ export default function Optionbar() {
       </div>
       <div className="optionbar__bottom">
         <div className="optionbar__content-container">
-          <select>
+          <select className="optionbar__bottom-option">
             <option value="Harv's Air">Harv's Air</option>
             <option value="Wwfc">Wwfc</option>
           </select>
-          <select>
+          <select className="optionbar__bottom-option">
             <option value="allAircraft">Aircraft(16)</option>
             <option value="FAKH">C-FAKH</option>
             <option value="GUZZ">C-GUZZ</option>
           </select>
-          <select>
+          <select className="optionbar__bottom-option">
             <option value="allInstructor">All instructors</option>
             <option value="josh">Josh</option>
             <option value="Jensen">Jensen</option>
           </select>
-          <select>
+          <select className="optionbar__bottom-option">
             <option value="noEquip">No Equipment</option>
             <option value="placeholder">Placeholder</option>
           </select>
-          <select>
+          <select className="optionbar__bottom-option">
             <option value="allActivities">All Activity Types</option>
             <option value="placeholder">Placeholder</option>
           </select>
-          <select>
+          <select className="optionbar__bottom-option">
             <option value="allReservations">All Reservations</option>
             <option value="placeholder">Placeholder</option>
           </select>
