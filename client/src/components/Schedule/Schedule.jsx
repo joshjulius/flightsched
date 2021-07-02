858f03feb26f0cae6304303a955945da6a1d5c0d
Joshua Julius <joshjulius1@gmail.com>
2021-07-02 17:14:09 -0500

    Update Schedule.jsx

--- client/src/components/Schedule/Schedule.jsx
+++ client/src/components/Schedule/Schedule.jsx
@@ -4,15 +4,8 @@ import Modal from "../../components/Modal/Modal";
 import Slot from "../Slot/Slot";
 import axios from "axios";
 
-export default function Schedule({
-  planes,
-  date,
-  user,
-  showBookingModal,
-  visibility,
-  hideModal,
-  filterValue,
-}) {
+export default function Schedule({ planes, date, showBookingModal, visibility, hideModal }) {
+  
   const [slots, setSlots] = useState([]);
   const [loading, setLoading] = useState(false);
   const slotsURL = `http://localhost:5000/api/slots/${date}`;
@@ -20,18 +13,7 @@ export default function Schedule({
   const axiosSlotsCall = async () => {
     try {
       const res = await axios.get(slotsURL);
-      let slotsData = res.data;
-      if (!filterValue || filterValue.instructor === "allInstructor") {
-        setSlots(res.data);
-      } else if (filterValue && filterValue.instructor) {
-        setSlots(
-          slotsData.filter((data) => data.instructor === filterValue.instructor)
-        );
-      } else if (filterValue && filterValue.aircraft) {
-        setSlots(
-          slotsData.filter((data) => data.aircraft === filterValue.aircraft)
-        );
-      }
+      setSlots(res.data);
       setLoading(false);
     } catch (err) {
       console.log(err);
@@ -41,8 +23,8 @@ export default function Schedule({
   useEffect(() => {
     axiosSlotsCall();
     setLoading(true);
-  }, [slotsURL, filterValue]);
-
+  }, [slotsURL]);
+  
   const timeHead = [];
   const planeSlot = [];
 
@@ -67,7 +49,7 @@ export default function Schedule({
   planeTimeSlot(8, "");
 
   const timeBlock = (slot) => {
-    return (
+    return(
       <Slot
         id={slot._id}
         location={slot.location}
@@ -75,7 +57,7 @@ export default function Schedule({
         endTime={slot.endTime}
         activityType={slot.activityType}
         aircraft={slot.aircraft}
-        instructor={slot.instructor}
+        instructor={slot.instructor} 
         customer={slot.customer}
         loading={loading}
         slotCall={axiosSlotsCall}
@@ -88,133 +70,7 @@ export default function Schedule({
         comments={slot.comments}
       />
     );
-  };
-
-  // const test = () => {
-  //   if (filterValue && filterValue.instructor === "Josh") {
-  //     return (
-  //       <>
-  //         <h2>Josh</h2>
-  //       </>
-  //     );
-  //   } else {
-  //     return (
-  //       <>
-  //         <h2>Jensen</h2>
-  //       </>
-  //     );
-  //   }
-  // };
-  const planesBlock = () => {
-    if (
-      !filterValue ||
-      (filterValue && filterValue.aircraft === "allAircraft") ||
-      !filterValue.aircraft
-    ) {
-      return (
-        <>
-          {planes.map((info) => {
-            return (
-              <tr className="schedule__row">
-                <th
-                  key={info._id}
-                  className={`schedule__placeholder ${info.reg}`}
-                >
-                  {`${info.reg} ${info.type}`}
-                  {slots
-                    .filter((slot) => slot.aircraft === info.reg)
-                    .map(timeBlock)}
-                </th>
-                {planeSlot}
-              </tr>
-            );
-          })}
-        </>
-      );
-    }
-    if (filterValue && filterValue !== "allAircraft") {
-      let plane = slots.filter(
-        (slot) => slot.aircraft === filterValue.aircraft
-      );
-      return (
-        <tr className="schedule__row">
-          <th
-            key={plane && plane[0]._id}
-            className={`schedule__placeholder ${plane && plane[0].aircraft}`}
-          >
-            {`${plane && plane[0].aircraft} ${plane && plane[0].type}`}
-            {slots
-              .filter((slot) => slot.aircraft === plane[0].aircraft)
-              .map(timeBlock)}
-          </th>
-          {planeSlot}
-        </tr>
-      );
-    }
-  };
-
-  const insturctorBlock = () => {
-    if (
-      !filterValue ||
-      (filterValue && filterValue.instructor === "allInstructor") ||
-      !filterValue.instructor
-    ) {
-      return (
-        <>
-          {user.map((info) => {
-            return (
-              <tr className="schedule__row">
-                <th
-                  key={info._id}
-                  className={`schedule__placeholder ${info && info.name}`}
-                >
-                  {info && info.name}
-                  {slots
-                    .filter((slot) => slot.instructor === info.name)
-                    .map(timeBlock)}
-                </th>
-                {planeSlot}
-              </tr>
-            );
-          })}
-        </>
-      );
-    }
-    if (filterValue && filterValue !== "allInstructor") {
-      let user = slots.filter(
-        (slot) => slot.instructor === filterValue.instructor
-      );
-      console.log(user);
-      return (
-        <tr className="schedule__row">
-          <th
-            key={user && user[0]._id}
-            className={`schedule__placeholder ${user && user[0].instructor}`}
-          >
-            {user && user[0].instructor}
-            {slots
-              .filter((slot) => slot.instructor === user[0].instructor)
-              .map(timeBlock)}
-          </th>
-          {planeSlot}
-        </tr>
-      );
-    }
-    if (
-      filterValue &&
-      !slots.filter((slot) => slot.instructor === user[0].instructor)
-    ) {
-      <tr className="schedule__row">
-        <th
-          key={user && user[0]._id}
-          className={`schedule__placeholder ${user && user[0].instructor}`}
-        >
-          {user && user[0].instructor}
-        </th>
-        {planeSlot}
-      </tr>;
-    }
-  };
+  }
 
   return (
     <>
@@ -233,83 +89,39 @@ export default function Schedule({
           </tr>
         </thead>
         <tbody>
-
           {
             planes && planes.map((info) => {
               const checkReg = (slot) => {
                 return slot.aircraft === `${info.reg} ${info.type}`;
-
-          {planes && planesBlock()}
-          {/* {planes &&
-            planes.map((info) => {
-              // const checkReg = (slot) => {
-              //   if (
-              //     !filterValue ||
-              //     (filterValue && filterValue.aircraft === "allAircraft") ||
-              //     !filterValue.aircraft
-              //   ) {
-              //     return slot.aircraft === `${info.reg} ${info.type}`;
-              //   }
-              // };
-              if (
-                !filterValue ||
-                (filterValue && filterValue.aircraft === "allAircraft") ||
-                !filterValue.aircraft
-              ) {
-                return (
-                  <tr className="schedule__row">
-                    <th
-                      key={info._id}
-                      className={`schedule__placeholder ${info.reg}`}
-                    >
-                      {`${info.reg} ${info.type}`}
-                      {slots
-                        .filter((slot) => slot.aircraft === `${info.reg} ${info.type}`)
-                        .map(timeBlock)}
-                    </th>
-                    {planeSlot}
-                  </tr>
-                );
-
               }
-              if (filterValue && filterValue.aircraft) {
-                return (
-                  <tr className="schedule__row">
-                    <th
-                      key={info._id}
-                      className={`schedule__placeholder ${info.reg}`}
-                    >
-                      {`${info.reg} ${info.type}`}
-                      {slots
-                        .filter(
-                          (slot) => slot.aircraft === filterValue.aircraft
-                        )
-                        .map(timeBlock)}
-                    </th>
-                    {planeSlot}
-                  </tr>
-                );
-              }
-            })} */}
-          {user && insturctorBlock()}
-          {/* <tr className="schedule__row">
+              return (
+                <tr className="schedule__row">
+                  <th
+                    key={info._id}
+                    className={`schedule__placeholder ${info.reg}`}
+                  >
+                    {`${info.reg} ${info.type}`}
+                    {slots.filter(checkReg).map(timeBlock)}
+                  </th>
+                  {planeSlot}
+                </tr>
+              );
+            })
+          }
+          <tr className="schedule__row">
             <th className="schedule__placeholder">
               Jensen
-              {slots
-                .filter((slot) => slot.instructor === "Jensen")
-                .map(timeBlock)}
+              {slots.filter(slot => slot.instructor === "Jensen").map(timeBlock)}
             </th>
             {planeSlot}
           </tr>
           <tr className="schedule__row">
             <th className="schedule__placeholder">
               Josh
-              {slots
-                .filter((slot) => slot.instructor === "Josh")
-                .map(timeBlock)}
+              {slots.filter(slot => slot.instructor === "Josh").map(timeBlock)}
             </th>
             {planeSlot}
-          </tr> */}
+          </tr>
         </tbody>
       </table>
     </>
